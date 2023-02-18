196d1f83e48e1fcd80d3b37fa46487a979a09f32
muhammadiwa <muhammad.iwa94@gmail.com>
2023-02-18 20:35:16 +0700

    add pencarian buku

--- resources/views/book-list.blade.php
+++ resources/views/book-list.blade.php
@@ -3,6 +3,29 @@
 @section('title', 'Dashboard')
 
 @section('content')
+<form action="" method="get">
+    <div class="row">
+        <div class="col-12 col-sm-6">
+            <select name="category" id="category" class="form-control">
+                <option value="">Select Category</option>
+                @foreach ($categories as $item)
+                    <option value="{{ $item->id }}">{{ $item->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="col-12 col-sm-6">
+            <div class="input-group mb-3">
+                <input type="text" name="title" id="" class="form-control" placeholder="Search Books Title">
+                <button class="btn btn-primary" type="submit">Search</button>
+            </div>
+        </div>
+    </div>
+</form>
+<div class="row">
+    <div class="col-12 col-sm-6">
+
+    </div>
+</div>
     <div class="my-2">
         <div class="row">
             @foreach ($books as $item)
